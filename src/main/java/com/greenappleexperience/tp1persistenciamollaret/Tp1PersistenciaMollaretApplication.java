731a6b9ce3d5b178bed738b8a095e2d13d4cd630
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-09-12 19:18:53 -0300

    Add - Relations

---
+++ src/main/java/com/greenappleexperience/tp1persistenciamollaret/Tp1PersistenciaMollaretApplication.java
@@ -0,0 +1,14 @@
+package com.greenappleexperience.tp1persistenciamollaret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1PersistenciaMollaretApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1PersistenciaMollaretApplication.class, args);
+
+
+    }
+}
